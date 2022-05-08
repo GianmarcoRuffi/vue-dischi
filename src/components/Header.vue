d89499f5291668c5ff819ce401cc9a4ecdc6d9c6
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-08 22:44:59 +0200

    Prova 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,30 @@
-<template></template>
+<template>
+  <header class="d-flex justify-content-space-between align-items-center">
+    <div class="logo">
+      <img
+        src="https://grafica-facile.com/wp-content/uploads/2022/01/spotify-logo.png"
+        alt="logo"
+        class="img-fluid"
+      />
+    </div>
+  </header>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "Header",
+};
 </script>
 
-<style></style>
+<style lang="scss" scope>
+@import "@/assets/vars.scss";
+header {
+  background-color: #2e3a46;
+  height: 70px;
+  .logo {
+    width: 50px;
+    height: 50px;
+    margin-left: 30px;
+  }
+}
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,7 +1,67 @@
-<template></template>
+<template>
+  <section class="container">
+    <div class="album-container row py-3 d-flex justify-content-center">
+      <div class="col-3">
+        <div
+          v-for="(item, index) in albumList"
+          :key="index"
+          class="card justify-content-center align-items-center"
+        >
+          <img class="img-fluid" :src="item.poster" :alt="item.title" />
+          <div class="title-card">{{ item.title }}</div>
+          <div class="text-card">{{ item.author }}</div>
+          <div class="text-card">{{ item.genre }}</div>
+          <div class="text-card">{{ item.year }}</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  name: "MainContent",
+  data() {
+    return {
+      albumList: [],
+      apiPath: "https://flynn.boolean.careers/exercises/api/array/",
+    };
+  },
+  mounted() {
+    axios
+      .get(this.apiPath + "music")
+      .then((res) => {
+        console.log(res);
+        this.albumList = res.data.response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scope>
+@import "@/assets/vars.scss";
+.album-container {
+  background: $bg-color;
+  width: 100%;
+  .card {
+    background: $bg-card-color;
+    text-transform: uppercase;
+    height: 500px;
+    img {
+      width: 100%;
+      height: auto;
+    }
+    .title-card {
+      color: $title-color;
+      font-weight: bold;
+    }
+    .text-card {
+      color: $text-color;
+    }
+  }
+}
+</style>
