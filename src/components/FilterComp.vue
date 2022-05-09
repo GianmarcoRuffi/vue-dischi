7fc384bd9897b58b85c5fdb862a3a6231e6d0e04
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-09 17:27:00 +0200

    Prova filtro

---
+++ src/components/FilterComp.vue
@@ -0,0 +1,32 @@
+<template>
+  <select @change="startFilter" v-model="key" name="">
+    <option value="All">All</option>
+    <option v-for="(genre, index) in genres" :value="genre" :key="index">
+      {{ genre }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "FilterComp",
+  props: {
+    genres: Array,
+  },
+  data() {
+    return {
+      key: "All",
+    };
+  },
+  methods: {
+    startFilter() {
+      this.$emit("startFilter", this.key);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/vars.scss";
+@import "@/assets/general.scss";
+</style>
